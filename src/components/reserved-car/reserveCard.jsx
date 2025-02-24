62947a68c1385235866876a8952f6a5eb106a004
Reza matini <rezamatini133@gmail.com>
2025-02-24 15:30:22 +0330

    final step and complete

--- src/components/reserved-car/reserveCard.jsx
+++ src/components/reserved-car/reserveCard.jsx
@@ -1,92 +1,108 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 const Data = [
   {
     src: "/svg/reservedCar/benz.svg",
-    name: "بنز e350",
+    name: "بنز e350 اجاره",
     model: "مدل:2016",
-    date: "از  ۱ تا 30 روز:8,500,000",
+    date: "از  ۱ تا ۳۰ روز:",
+    datePrice: "۸,۵۰۰,۰۰۰",
     dayli: "روزانه",
     monthly: "ماهانه",
-    priceTitle: "مبلغ ضمانت",
-    price: "80 میلیون تومان",
+    priceTitle: "مبلغ ضمانت:",
+    price: "۸۰ میلیون تومان",
   },
   {
     src: "/svg/reservedCar/land.svg",
-    name: "لندکروز",
+    name: "اجاره لندکروز",
     model: "مدل:2016",
-    date: "از  ۱ تا 30 روز:8,500,000",
+    date: "از  ۱ تا ۳۰ روز:",
+    datePrice: "۸,۵۰۰,۰۰۰",
     dayli: "روزانه",
     monthly: "ماهانه",
-    priceTitle: "مبلغ ضمانت",
-    price: "80 میلیون تومان",
+    priceTitle: "مبلغ ضمانت:",
+    price: "۸۰ میلیون تومان",
   },
   {
     src: "/svg/reservedCar/jenesis.svg",
-    name: "جنسیس کوپه",
+    name: "اجاره جنسیس کوپه",
     model: "مدل:2016",
-    date: "از  ۱ تا 30 روز:8,500,000",
+    date: "از  ۱ تا ۳۰ روز:",
+    datePrice: "۸,۵۰۰,۰۰۰",
     dayli: "روزانه",
     monthly: "ماهانه",
-    priceTitle: "مبلغ ضمانت",
-    price: "80 میلیون تومان",
+    priceTitle: "مبلغ ضمانت:",
+    price: "۸۰ میلیون تومان",
   },
   {
     src: "/svg/reservedCar/bmw.svg",
-    name: "بی ام او",
+    name: "اجاره بی ام او",
     model: "مدل:2016",
-    date: "از  ۱ تا 30 روز:8,500,000",
+    date: "از  ۱ تا ۳۰ روز:",
+    datePrice: "۸,۵۰۰,۰۰۰",
     dayli: "روزانه",
     monthly: "ماهانه",
-    priceTitle: "مبلغ ضمانت",
-    price: "80 میلیون تومان",
+    priceTitle: "مبلغ ضمانت:",
+    price: "۸۰ میلیون تومان",
   },
   {
     src: "/svg/reservedCar/tucsan.svg",
-    name: "هیوندای توسان",
+    name: "اجاره هیوندای توسان",
     model: "مدل:2016",
-    date: "از  ۱ تا 30 روز:8,500,000",
+    date: "از  ۱ تا ۳۰ روز:",
+    datePrice: "۸,۵۰۰,۰۰۰",
     dayli: "روزانه",
     monthly: "ماهانه",
-    priceTitle: "مبلغ ضمانت",
-    price: "80 میلیون تومان",
+    priceTitle: "مبلغ ضمانت:",
+    price: "۸۰ میلیون تومان",
   },
   {
     src: "/svg/reservedCar/maserati.svg",
-    name: "مازراتی گرندکوپه",
+    name: "اجاره مازراتی گرندکوپه",
     model: "مدل:2016",
-    date: "از  ۱ تا 30 روز:8,500,000",
+    date: "از  ۱ تا ۳۰ روز:",
+    datePrice: "۸,۵۰۰,۰۰۰",
     dayli: "روزانه",
     monthly: "ماهانه",
-    priceTitle: "مبلغ ضمانت",
-    price: "80 میلیون تومان",
+    priceTitle: "مبلغ ضمانت:",
+    price: "۸۰ میلیون تومان",
   },
 ];
 
 export const ReserveCard = () => {
   return (
-    <div className="grid grid-cols-3 gap-6 place-items-center">
+    <div className="grid grid-cols-3 gap-5 place-items-center">
       {Data.map((item, index) => (
-        <div key={index}>
+        <div key={index} className="rounded-xl border-2 border-gray-200 p-6">
           <div>
             <img src={item.src} alt={item.name} />
           </div>
-          <div>
+          <div className="flex flex-col text-xl font-bold">
             {item.name}
-            <span>{item.model}</span>
+            <span className="text-base font-normal text-gray-400">
+              {item.model}
+            </span>
           </div>
-          <div>
+          <div className="flex justify-between text-lg font-normal text-gray-400 gap-1">
             {item.date}
+            <span className="text-lg font-bold ml-[8rem] text-blue-600">
+              {item.datePrice}
+            </span>
             <span>{item.dayli}</span>
           </div>
-          <div>
+          <div className="flex justify-between mt-5 mb-14 text-lg font-normal text-gray-400 gap-1">
             {item.date}
+            <span className="text-lg font-bold ml-[7.5rem] text-blue-600">
+              {item.datePrice}
+            </span>
             <span>{item.monthly}</span>
           </div>
-          <div>
+          <div className="flex justify-between border-t-2 border-gray-300 py-4 text-lg ">
             {item.priceTitle}
-            <span>{item.price}</span>
+            <span className="font-bold">{item.price}</span>
           </div>
+          <Button className="w-full mt-1 bg-blue-600">درخواست رزرو</Button>
         </div>
       ))}
     </div>
